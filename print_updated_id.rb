8af651fdfd78cf5d8bb5f9c9260984ab4e70fa62
UseK <use.k.james@gmail.com>
2013-01-24 13:35:01 +0900

    refactoring print_updated_id.rb

--- print_updated_id.rb
+++ print_updated_id.rb
@@ -1,16 +1,17 @@
 #encoding:utf-8
+require "pp"
 if ARGV[1].nil?
 	puts "Usage: ruby update_saturday.rb id.txt saturday.txt"
 	exit
 end
 saturday = File.open(ARGV[1], "r")
-File.open(ARGV[0], "r").each_line do |line|
+id = File.open(ARGV[0], "r").read.split(/\n/).map {|line| line.split(/,/)[0..2] }
+id.each do |item|
 	day = saturday.gets
 	if !day
 		saturday.rewind
 		day = saturday.gets
 	end
-	item = line.split(/,/)
-	print item[0..2].join(",").chomp, ",", day
+	print item.join(",").chomp, ",", day
 end
 
