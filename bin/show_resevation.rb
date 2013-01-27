472c032e48674fa5c8569eeebeccbdc021256b38
UseK <use.k.james@gmail.com>
2013-01-27 20:36:26 +0900

    出力ファイルのスペルミスを修正

--- bin/show_resevation.rb
+++ bin/show_resevation.rb
@@ -3,7 +3,7 @@ root_dir = File.dirname(__FILE__) + "/../"
 $LOAD_PATH << root_dir + "lib"
 require "resevation"
 csv_account_path = root_dir + "input/account.csv"
-output_path = root_dir + "output/resavation.txt"
+output_path = root_dir + "output/resevation.txt"
 puts "予約情報を#{output_path} へ保存します"
 Resevation.new.show_all csv_account_path, output_path
 puts "予約情報を#{output_path} へ保存しました"
